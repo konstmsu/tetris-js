e634cb8a84899f73bd0862a2f49db7b662e4eb9a
Konstantin Spirin <konstmsu@gmail.com>
2021-08-09 18:07:47 +0800

    Use all figures in game

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,4 @@
-import { every, floor, pullAt, range } from "lodash";
+import { every, floor, pullAt, range, sample } from "lodash";
 import { Cell, XY } from "./core";
 import { Figure, Position, PositionedFigure } from "./figure";
 import { delay } from "./utils";
@@ -73,7 +73,9 @@ export class FallingFigure {
   }
 
   readonly spawn = (): void => {
-    const measuredFigure = new PositionedFigure(Figure.I);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const figure = sample([Figure.I, Figure.O, Figure.S, Figure.T, Figure.Z])!;
+    const measuredFigure = new PositionedFigure(figure);
 
     this.figure = new PositionedFigure(measuredFigure.figure, {
       rotations: 0,
