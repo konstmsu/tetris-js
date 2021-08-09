9c5574fe171447650eeb03264ad948b66630cb1a
Konstantin Spirin <konstmsu@gmail.com>
2021-08-09 16:54:07 +0800

    Lint fix

--- src/utils.ts
+++ src/utils.ts
@@ -8,5 +8,6 @@ export const getSize = (blocks: ReadonlyArray<XY>): XY => {
   const xs = blocks.map((b) => b.x);
   const ys = blocks.map((b) => b.y);
 
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return { x: max(xs)! - min(xs)! + 1, y: max(ys)! - min(ys)! + 1 };
 };
